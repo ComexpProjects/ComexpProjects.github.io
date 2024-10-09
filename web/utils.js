42e56552a8eef2f497eca824c43727fd21e3d3cf
ressiwage <ressiwage@ya.ru>
2024-10-09 13:54:10 +0500

    delete issue

--- web/utils.js
+++ web/utils.js
@@ -1,6 +1,6 @@
 function deleteArchive(url, id) {
     return fetch(url, {
-        method: 'POST',
+        method: 'DELETE',
         body: JSON.stringify({'archive_id':id}),
         headers: {
             'archives-token': JSON.parse(localStorage.getItem('comexp-token')),
